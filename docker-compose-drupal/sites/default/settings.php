a93004314aac6b37c7f513de232c8e61a32ae158
screwyterror83 <screwyterror83@gmail.com>
2018-06-15 20:55:13 -0700

    create ignore file and docker-compose file

---
+++ docker-compose-drupal/sites/default/settings.php
@@ -0,0 +1,14 @@
+<?php
+$databases['default']['default'] = array (
+  'database' => 'postgres',
+  'username' => 'postgres',
+  'password' => 'testing123',
+  'prefix' => '',
+  'host' => 'postgres',
+  'port' => '5432',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\pgsql',
+  'driver' => 'pgsql',
+);
+$settings['hash_salt'] = '1Yy22SJbgfoLxXlblQQVT-E2TJKVUHjN3hg2APdjq1DaTemyqqMbcFZUVpnx6clp2znTzQ3J3Q';
+$settings['install_profile'] = 'standard';
+$config_directories['sync'] = 'sites/default/files/config_ewPEBb6JOowBD-FryGTej1mzmVtYzhbCDLM0KzkN-8v62bDEKlzJkVU65XXCdrUdJgmUuIj2Vw/sync';
